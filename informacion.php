3a211e60a647be0cdd67461c30ea806adbe8ea86
JSEBASRT22 <sebas002xd@gmail.com>
2024-08-06 14:47:11 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,10 @@
-
+<?php
+$Identificacion=$_POST ['Identificacion'];
+$NomApellido=$_POST ['NomApellido'];
+$Cargo=$_POST ['Cargo'];
+$Salario=$_POST ['Salario'];
+echo "La identificacion del empleado es: ".Identificacion;
+echo "Los nombres y apellidos del empleado es: ".NomApellido;
+echo "La cargo del empleado es: ".Cargo;
+echo "La salario del empleado es: ".Salario;
+?>
